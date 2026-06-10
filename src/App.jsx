540812f7b2b5195137fa7c07b0f13c98e78c09e7
Swapnil Mundaware <93301234+SwapzzzGit@users.noreply.github.com>
2026-06-10 15:05:57 +0530

    5: navbar with scss and react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import ProductListing from './pages/ProductListing'
 import ProductDetail from './pages/ProductDetail'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductListing />} />
         <Route path="/product/:id" element={<ProductDetail />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { FiMenu, FiSearch, FiShoppingCart, FiUser, FiUsers } from 'react-icons/fi'
+import './Navbar.scss'
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
+    <nav className="navbar">
+      <div className="navbar-inner">
+
+        <button className="nav-menu-btn" aria-label="Menu">
+          <FiMenu size={22} color="#fff" />
+        </button>
+
+        <div className="nav-search">
+          <FiSearch size={16} />
+          <input type="text" placeholder="Search products..." />
+        </div>
+
+        <div className="nav-icons">
+          <button aria-label="Cart"><FiShoppingCart size={22} /></button>
+          <button aria-label="Account"><FiUser size={22} /></button>
+          <button aria-label="Profile"><FiUsers size={22} /></button>
+        </div>
+
+      </div>
     </nav>
-  );
+  )
 }
